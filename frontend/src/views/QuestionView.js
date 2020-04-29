4215c0bafec2761ecb370fd3891c79a7380639fc
Agata Kosior <agata.kosior57@gmail.com>
2020-04-29 13:55:37 +0200

    Change jQuery for axios in QuestionView

--- frontend/src/views/QuestionView.js
+++ frontend/src/views/QuestionView.js
@@ -1,7 +1,7 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import Question from '../components/Question';
 import Pagination from '../components/Pagination';
-import $ from 'jquery';
+import axios from 'axios';
 import { config } from '../config';
 
 import '../stylesheets/App.css';
@@ -9,152 +9,104 @@ import Categories from '../components/Categories';
 
 const { BASE_URL } = config;
 
-class QuestionView extends Component {
-  constructor() {
-    super();
-    this.state = {
-      questions: [],
-      page: 1,
-      totalQuestions: 0,
-      categories: [],
-      currentCategory: null,
-    };
-  }
+const QuestionView = () => {
+  const [questions, setQuestions] = useState([]);
+  const [page, setPage] = useState(1);
+  const [totalQuestions, setTotalQuestions] = useState(0);
+  const [categories, setCategories] = useState([]);
+  const [currentCategory, setCurrentCategory] = useState(null);
+  const [error, setError] = useState('');
 
-  componentDidMount() {
-    this.getQuestions();
-  }
+  useEffect(() => {
+    getQuestions();
+  }, [page]);
 
-  getQuestions = () => {
-    $.ajax({
-      url: `${BASE_URL}/questions?page=${this.state.page}`,
-      type: 'GET',
-      success: (result) => {
-        this.setState({
-          questions: result.questions,
-          totalQuestions: result.total_questions,
-          categories: result.categories,
-          currentCategory: result.current_category,
-        });
-        return;
-      },
-      error: (error) => {
-        alert('Unable to load questions. Please try your request again');
-        return;
-      },
-    });
-  };
-
-  selectPage = (num) => {
-    this.setState({ page: num }, () => this.getQuestions());
-  };
-
-  getByCategory = (id) => {
-    $.ajax({
-      url: `${BASE_URL}/categories/${id}/questions`,
-      type: 'GET',
-      success: (result) => {
-        this.setState({
-          questions: result.questions,
-          totalQuestions: result.total_questions,
-          currentCategory: result.current_category,
-        });
-        return;
-      },
-      error: (error) => {
-        alert('Unable to load questions. Please try your request again');
-        return;
-      },
-    });
-  };
-
-  submitSearch = (searchTerm) => {
-    $.ajax({
-      url: `${BASE_URL}/questions/search`,
-      type: 'POST',
-      dataType: 'json',
-      contentType: 'application/json',
-      data: JSON.stringify({ search_term: searchTerm }),
-      xhrFields: {
-        withCredentials: true,
-      },
-      crossDomain: true,
-      success: (result) => {
-        this.setState({
-          questions: result.questions,
-          totalQuestions: result.total_questions,
-          currentCategory: result.current_category,
-        });
-        return;
-      },
-      error: (error) => {
-        alert('Unable to load questions. Please try your request again');
-        return;
-      },
-    });
-  };
-
-  questionAction = (id) => (action) => {
-    if (action === 'DELETE') {
-      if (window.confirm('are you sure you want to delete the question?')) {
-        $.ajax({
-          url: `${BASE_URL}/questions/${id}`,
-          type: 'DELETE',
-          success: (result) => {
-            if (this.state.questions.length === 1) {
-              this.setState(
-                (prevState) => ({
-                  page: prevState.page - 1,
-                }),
-                () => this.getQuestions()
-              );
-            } else {
-              this.getQuestions();
-            }
-          },
-          error: (error) => {
-            alert('Unable to load questions. Please try your request again');
-            return;
-          },
-        });
-      }
+  const getQuestions = async () => {
+    try {
+      const { data } = await axios.get(`${BASE_URL}/questions?page=${page}`);
+      setQuestions(data.questions);
+      setTotalQuestions(data.total_questions);
+      setCategories(data.categories);
+      setCurrentCategory(data.current_category);
+    } catch (error) {
+      setError(error);
     }
   };
 
-  render() {
-    return (
-      <div className='question-view'>
-        <Categories
-          categories={this.state.categories}
-          getQuestions={this.getQuestions}
-          submitSearch={this.submitSearch}
-          getByCategory={this.getByCategory}
-        />
-        <div className='questions-list'>
-          <h2>Questions</h2>
-          {this.state.questions.map((q, ind) => (
-            <Question
-              key={q.id}
-              question={q.question}
-              answer={q.answer}
-              category={
-                this.state.categories.find(
-                  (category) => category.id === q.category
-                ).type
-              }
-              difficulty={q.difficulty}
-              questionAction={this.questionAction(q.id)}
-            />
-          ))}
-          <Pagination
-            totalQuestions={this.state.totalQuestions}
-            page={this.state.page}
-            selectPage={this.selectPage}
+  const selectPage = (num) => {
+    setPage(num);
+  };
+
+  const getByCategory = async (id) => {
+    try {
+      const { data } = await axios.get(
+        `${BASE_URL}/categories/${id}/questions`
+      );
+      setQuestions(data.questions);
+      setTotalQuestions(data.total_questions);
+      setCurrentCategory(data.current_category);
+    } catch (error) {
+      setError(error);
+    }
+  };
+
+  const submitSearch = async (searchTerm) => {
+    try {
+      const { data } = await axios.post(`${BASE_URL}/questions/search`, {
+        search_term: searchTerm,
+      });
+      setQuestions(data.questions);
+      setTotalQuestions(data.total_questions);
+      setCurrentCategory(data.current_category);
+    } catch (error) {
+      setError(error);
+    }
+  };
+
+  const questionAction = (id) => async (action) => {
+    if (action === 'DELETE') {
+      if (window.confirm('are you sure you want to delete the question?')) {
+        await axios.delete(`${BASE_URL}/questions/${id}`);
+
+        if (questions.length === 1) {
+          setPage(page - 1)
+        } else {
+          getQuestions()
+        }
+      }
+    }
+  };
+  return (
+    <div className='question-view'>
+      <Categories
+        categories={categories}
+        currentCategory={currentCategory}
+        getQuestions={getQuestions}
+        submitSearch={submitSearch}
+        getByCategory={getByCategory}
+      />
+      <div className='questions-list'>
+        <h2>Questions</h2>
+        {questions.map((q) => (
+          <Question
+            key={q.id}
+            question={q.question}
+            answer={q.answer}
+            category={
+              categories.find((category) => category.id === q.category)?.type
+            }
+            difficulty={q.difficulty}
+            questionAction={questionAction(q.id)}
           />
-        </div>
+        ))}
+        <Pagination
+          totalQuestions={totalQuestions}
+          page={page}
+          selectPage={selectPage}
+        />
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
 
 export default QuestionView;
